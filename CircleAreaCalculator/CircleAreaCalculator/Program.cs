251c602629b10e1ef2fdf3c76ae7777770c0f905
Lucas Araujo <lsa1@live.com.ar>
2022-11-14 11:26:34 -0300

    App calculadora area de circulo

---
+++ CircleAreaCalculator/CircleAreaCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CircleAreaCalculator
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.1416;
+            string userData;
+            double radius;
+            double circleArea;
+
+            Console.WriteLine("proporciona el radio de tu circulo");
+            userData = Console.ReadLine();
+
+            if (double.TryParse(userData, out radius))
+            {
+                circleArea = PI * Math.Pow(radius, 2);
+                Console.WriteLine($"El area de tu circulo es: {circleArea}");
+            }
+            else
+            {
+                Console.WriteLine("los datos proporcionaos no son correctos");
+                Console.ReadKey();
+
+            }
+
+        }
+
+    }
+
+}
