5eedac46624d231456f620ae6f6ab32d88f5616f
Lucas Araujo <lsa1@live.com.ar>
2022-11-14 20:02:50 -0300

    calculadora de area en triangulo correc

---
+++ TriangleAreaCalc/TriangleAreaCalc/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TriangleAreaCalculator
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string userData;
+            string userData2;
+            double width;
+            double height;
+            double TriangleArea;
+
+            Console.WriteLine("Proporciona la base de tu triangulo");
+            userData = Console.ReadLine();
+
+            Console.WriteLine("proporciona la altura de tu triangulo");
+            userData2 = Console.ReadLine();
+
+
+            if (double.TryParse(userData, out width) && double.TryParse(userData2, out height))
+            {
+                TriangleArea = (width * height) / 2 ;
+                Console.WriteLine($"El area de tu triangulo es: {TriangleArea}");
+            }
+            else
+            {
+                Console.WriteLine("los datos proporcionaos no son correctos");
+                Console.ReadKey();
+
+            }
+
+        }
+
+    }
+
+}
+
